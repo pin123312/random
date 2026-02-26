83a5781e5a6f01b4ba69ac5a1d1dae3de765b43c
o.o <115215908+pin123312@users.noreply.github.com>
2026-02-26 12:54:30 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -18,7 +7,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/random/">
     <App />
   </BrowserRouter>
 );
